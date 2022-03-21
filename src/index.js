200c8dceec9de8e25b839ef7a9c84c6eb2cdcbaa
daanba <81332010+daanba@users.noreply.github.com>
2022-03-21 17:27:25 +0100

    //It should display a h1 heading.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 //It should display a h1 heading.
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
